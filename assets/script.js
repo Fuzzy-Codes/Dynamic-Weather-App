6c04d8cafeb4916162cd2d0039c17232a9868f0b
alexc <alexcolwell13@gmail.com>
2023-08-13 12:48:51 -0500

    cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
-const currentDay = document.querySelector("#currentDay")
-const forecastCard = document.querySelectorAll("#forecastCard")
-const cityInput = document.querySelector("#cityInput")
-const btn = document.querySelector("#btn")
-const apiKey = '35dfb19446063c3fd5cf257c0ada1dde';
 const currentSearch = document.querySelector("#currentSearch")
+const historyList = document.querySelector("#historyList")
+const currentDay = document.querySelector("#currentDay")
+const cityInput = document.querySelector("#cityInput")
+const humidity = document.querySelector("#humidity")
 const icon = document.querySelector("#icon")
 const temp = document.querySelector("#temp")
 const wind = document.querySelector("#wind")
-const humidity = document.querySelector("#humidity")
-const historyList = document.querySelector("#historyList")
+const btn = document.querySelector("#btn")
+const forecastCard = document.querySelectorAll("#forecastCard")
+const apiKey = '35dfb19446063c3fd5cf257c0ada1dde';
 
 function fetchCurrentWeather(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + city + "&appid=" + apiKey + "&units=imperial")
@@ -16,11 +16,11 @@ function fetchCurrentWeather(city) {
             return res.json()
         })
         .then(function (data) {
-            console.log(data)
             const date = new Date()
             const month = date.getMonth() + 1
             const day = date.getDate()
             const year = date.getFullYear()
+
             currentSearch.innerHTML = data.name + " " + month + "/" + day + "/" + year
             icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
             icon.setAttribute("alt", data.weather[0].description)
@@ -36,10 +36,10 @@ function fetchForecastWeather(city) {
             return res.json()
         })
         .then(function (data) {
-            console.log(data)
             for (let i = 0; i < forecastCard.length; i++) {
                 forecastCard[i].innerHTML = ""
                 const index = i * 8 + 4
+
                 const date = new Date(data.list[index].dt * 1000)
                 const month = date.getMonth() + 1
                 const day = date.getDate()
@@ -53,6 +53,7 @@ function fetchForecastWeather(city) {
                 icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[index].weather[0].icon + "@2x.png")
                 icon.setAttribute("alt", data.list[index].weather[0].description)
                 forecastCard[i].append(icon)
+
                 const temp = document.createElement("p")
                 temp.innerHTML = "Temp: " + data.list[index].main.temp + "&#176F"
                 forecastCard[i].append(temp)
@@ -64,7 +65,6 @@ function fetchForecastWeather(city) {
                 const humidity = document.createElement("p")
                 humidity.innerHTML = "Humidity: " + data.list[index].main.humidity + "%"
                 forecastCard[i].append(humidity)
-
             }
         })
 }
